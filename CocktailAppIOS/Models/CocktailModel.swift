9e6131ba1a8d0d9f9aecdcb200933ddc64933051
fredjeeeuuuhhh <frederic.detender@student.hogent.be>
2023-12-14 19:24:43 +0100

    Tabview and first try on layout

---
+++ CocktailAppIOS/Models/CocktailModel.swift
@@ -0,0 +1,21 @@
+//
+//  CocktailModel.swift
+//  CocktailAppIOS
+//
+//  Created by Frederic de Tender on 14/12/2023.
+//
+
+import Foundation
+
+struct Cocktail{
+    var id: Int
+    var title: String
+    var thumbNail: String
+    var ingredients: [String]
+    var measurements: [String]
+    var instructions: String
+    var category: String
+    var alcoholicFilter: String
+    var glass: String
+    
+}
---
+++ CocktailAppIOS/Models/IngredientModel.swift
@@ -0,0 +1,16 @@
+//
+//  IngredientModel.swift
+//  CocktailAppIOS
+//
+//  Created by Frederic de Tender on 14/12/2023.
+//
+
+import Foundation
+
+struct IngredientModel{
+    var id: Int
+    var name: String
+    var description: String
+    var containsAlcohol: Bool
+    var type: String
+}
