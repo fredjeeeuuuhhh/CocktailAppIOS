9f0fb7651d98b40f57d5a18cd11ad2b0cc5c33fb
fredjeeeuuuhhh <frederic.detender@student.hogent.be>
2023-12-29 11:54:13 +0100

    Add: Mockdata for cocktails and ingredients

--- CocktailAppIOS/Models/CocktailModel.swift
+++ CocktailAppIOS/Models/CocktailModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Cocktail {
+struct Cocktail: Decodable {
     let idDrink: String
     let strDrink: String
     let strCategory: String?
@@ -47,4 +47,17 @@ struct Cocktail {
     let strMeasure15: String?
 }
 
+struct CocktailGetAllResponse{
+    let drinks: [Cocktail]
+}
 
+struct CocktailMockData{
+    static let sampleCocktail = Cocktail(idDrink: "1",strDrink: "Bacardi Cola", strCategory: "Cocktail",strAlcohol: "Alcohol", strGlass: "Cocktail glass", strInstructions: "1. Poor in a glass and mix", strDrinkThumb: "",strIngredient1: "Bacardi",strIngredient2: "Cola",strIngredient3: nil,strIngredient4:nil,strIngredient5:nil,strIngredient6:nil,strIngredient7:nil,strIngredient8:nil,strIngredient9:nil,strIngredient10:nil,strIngredient11:nil,strIngredient12:nil,strIngredient13:nil,strIngredient14:nil,strIngredient15:nil,strMeasure1: "1 oz", strMeasure2: "Can",strMeasure3: nil,strMeasure4: nil,strMeasure5: nil,strMeasure6: nil,strMeasure7: nil,strMeasure8: nil,strMeasure9: nil,strMeasure10: nil,strMeasure11: nil,strMeasure12: nil,strMeasure13: nil,strMeasure14: nil,strMeasure15: nil)
+    
+    static let sampleCocktails = [
+        sampleCocktail,
+        Cocktail(idDrink: "2",strDrink: "Vodka Redbull", strCategory: "Cocktail",strAlcohol: "Alcohol", strGlass: "Cocktail glass", strInstructions: "1. Poor in a glass and mix", strDrinkThumb: "",strIngredient1: "Vodka",strIngredient2: "Redbull",strIngredient3: nil,strIngredient4:nil,strIngredient5:nil,strIngredient6:nil,strIngredient7:nil,strIngredient8:nil,strIngredient9:nil,strIngredient10:nil,strIngredient11:nil,strIngredient12:nil,strIngredient13:nil,strIngredient14:nil,strIngredient15:nil,strMeasure1: "1 oz", strMeasure2: "Can",strMeasure3: nil,strMeasure4: nil,strMeasure5: nil,strMeasure6: nil,strMeasure7: nil,strMeasure8: nil,strMeasure9: nil,strMeasure10: nil,strMeasure11: nil,strMeasure12: nil,strMeasure13: nil,strMeasure14: nil,strMeasure15: nil)
+        ,
+        Cocktail(idDrink: "3",strDrink: "Rum Cola", strCategory: "Cocktail",strAlcohol: "Alcohol", strGlass: "Cocktail glass", strInstructions: "1. Poor in a glass and mix", strDrinkThumb: "",strIngredient1: "Rum",strIngredient2: "Cola",strIngredient3: nil,strIngredient4:nil,strIngredient5:nil,strIngredient6:nil,strIngredient7:nil,strIngredient8:nil,strIngredient9:nil,strIngredient10:nil,strIngredient11:nil,strIngredient12:nil,strIngredient13:nil,strIngredient14:nil,strIngredient15:nil,strMeasure1: "1 oz", strMeasure2: "Can",strMeasure3: nil,strMeasure4: nil,strMeasure5: nil,strMeasure6: nil,strMeasure7: nil,strMeasure8: nil,strMeasure9: nil,strMeasure10: nil,strMeasure11: nil,strMeasure12: nil,strMeasure13: nil,strMeasure14: nil,strMeasure15: nil)
+    ]
+}
--- CocktailAppIOS/Models/IngredientModel.swift
+++ CocktailAppIOS/Models/IngredientModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Ingredient{
+struct Ingredient: Decodable{
     let idIngredient: String
     let strIngredient: String
     let strDescription: String?
@@ -16,3 +16,17 @@ struct Ingredient{
     let strABV: String?
 }
 
+struct IngredientResponse{
+    let ingredients: [Ingredient]
+}
+
+struct IngredientMockData{
+    static let sampleIngredient = Ingredient(idIngredient: "1", strIngredient: "Cola", strDescription: "Coca Cola 33cl", strType: "Soda", strAlcohol: "No", strABV: nil)
+    
+    static let sampleIngredients = [
+        sampleIngredient,
+        Ingredient(idIngredient: "2", strIngredient: "Bacardi", strDescription: "Bacardi white", strType: "Bacardi", strAlcohol: "Yes", strABV: "40"),
+        Ingredient(idIngredient: "3", strIngredient: "Redbull", strDescription: "Gives you wings", strType: "Soda", strAlcohol: "No", strABV: nil),
+        Ingredient(idIngredient: "4", strIngredient: "Vodka", strDescription: "Eristof Vodka white", strType: "Vodka", strAlcohol: "Yes", strABV: "40")
+    ]
+}
